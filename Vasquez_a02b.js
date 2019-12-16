d8851e4e116c25005b3873700b89c6e31c2ca47f
BryanJVasquez <58918516+BryanJVasquez@users.noreply.github.com>
2019-12-15 19:47:19 -0600

    Add files via upload

---
+++ Vasquez_a02b.js
@@ -0,0 +1,17 @@
+/*
+Bryan Vasquez
+Vasquez_a02b.js
+INFO 2124
+Thoendel
+12/15/2019
+*/
+let firtsName = "Bryan Vasquez";
+var age = 23;
+const hairColor = "Black";
+const eyeColor = "Brown";
+ console.log("About me v.1.0");
+ console.log("===============");
+ console.log("My name: " + firtsName);
+ console.log("My age: " + age);
+ console.log("My hair color: " + hairColor);
+ console.log("My eye color: " + eyeColor);
